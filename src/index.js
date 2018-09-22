1fc66393020df8e1302ba605bcfb485dc8f449d6
Anton <huliovalente@gmail.com>
2018-09-22 21:33:01 +0300

    fix
    add a little piece of code

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    getLoveTrianglesCount = function (integers) {
+        if (integers.length % 3 != 0) {
+            return 0;
+        }
+
+        var trianglesCount = 0;
+        for (var i = 0; i < integers.length; i++) {
+            if ((integers[i + 1] - integers[i] == 1) && integers[i + 2] == i + 1) {
+                trianglesCount++;
+            } else {
+                continue;
+            }
+        }
 };
